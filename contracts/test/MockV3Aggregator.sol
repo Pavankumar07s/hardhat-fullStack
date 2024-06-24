1c4b5abcd25f1f3a02821b6f612653c138c4f25e
Pavankumar07s <pawankumar14662693@gmail.com>
2024-06-24 18:08:22 +0530

    started deploying for Mocks for local net

---
+++ contracts/test/MockV3Aggregator.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
+
+
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,5 @@
+const { version } = require("chai");
+
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require('hardhat-deploy');
@@ -7,7 +9,10 @@ const SEPHOLIA_RPC_URL = process.env.SEPHOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 module.exports = {
-  solidity: "0.8.8",
+  // solidity: "0.8.8",
+  solidity:{
+    compilers:[{version:"0.8.8"},{version:"0.6.6"}]
+  },
   networks: {
     sepholia: {
       url: SEPHOLIA_RPC_URL,
